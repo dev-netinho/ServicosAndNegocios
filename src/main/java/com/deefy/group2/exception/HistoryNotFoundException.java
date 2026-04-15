816812dea03a9d4cd48f7f4e6456650ea44152c5
LucaasHenrique <lucashsouza101@gmail.com>
2026-04-15 15:34:42 -0300

    feat: add custom exception classes for handling not found scenarios

---
+++ src/main/java/com/deefy/group2/exception/HistoryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.deefy.group2.exception;
+
+public class HistoryNotFoundException extends RuntimeException {
+    public HistoryNotFoundException(String message) {
+        super(message);
+    }
+}
